260639e8fff4f0eb5b77510ebda163610a04cbd1
x86_64 <x86mail@gmail.com>
2012-07-21 13:09:40 +0400

    Fix build_error script

--- src/errors_list.c
+++ src/errors_list.c
@@ -1,13 +1,12 @@
 
 static err_item errs_list[] = {
- { -263, "src/main_mustache.c: mustache_render failed" },
- { -257, "src/main_mustache.c: buffer not supplied" },
- { -247, "src/main_mustache.c: bad precompile" },
- { -240, "src/main_mustache.c: bad template" },
- { -212, "src/main_mustache.c: calloc failed" },
- { -75, "src/main_mustache.c: mustache section render failed" },
+ { -463, "src/main_mustache.c: mustache_render failed" },
+ { -457, "src/main_mustache.c: buffer not supplied" },
+ { -447, "src/main_mustache.c: bad precompile" },
+ { -440, "src/main_mustache.c: bad template" },
+ { -412, "src/main_mustache.c: calloc failed" },
+ { -275, "src/main_mustache.c: mustache section render failed" },
 
-	{ 0, NULL }
 };
 #define            errs_list_size      sizeof(errs_list[0])
 #define            errs_list_nelements sizeof(errs_list) / errs_list_size
